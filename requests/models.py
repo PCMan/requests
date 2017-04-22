4b221638154e012388a27ca058cf3de7906b2ef5
Hong Jen Yee (PCMan) <pcman.tw@gmail.com>
2017-04-22 20:53:40 +0800

    Remove an extra space.

--- requests/models.py
+++ requests/models.py
@@ -833,7 +833,7 @@ class Response(object):
             #
             # If we're using `splitlines()`, we only do this if the chunk
             # ended midway through a line.
-            incomplete_line = lines[-1] and  lines[-1][-1] == chunk[-1]
+            incomplete_line = lines[-1] and lines[-1][-1] == chunk[-1]
             if delimiter or incomplete_line:
                 pending = lines.pop()
 
